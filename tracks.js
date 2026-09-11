ce9707b53aee297c9b932e0671cf5442d522a2fa
yuuuh26 <dengana.10011212@gmail.com>
2026-09-12 01:48:51 +0900

    Add Sunlight on the Dash custom track slot

--- tracks.js
+++ tracks.js
@@ -4,5 +4,6 @@ export const TRACKS = [
  {id:'city',name:'City Groove',bpm:118,style:'Groove / Funk',wave:'triangle',roots:[45,50,43,52],chord:[0,3,7,10],kick:[0,6,8,14],snare:[4,12],hat:[0,2,3,4,6,8,10,11,12,14],bass:[0,3,6,8,10,14],melody:[null,7,null,null,null,10,null,12,null,null,null,7,null,3,null,null],swing:.08},
  {id:'pulse',name:'Study Pulse',bpm:120,style:'Light Electronic',wave:'sine',roots:[48,56,51,58],chord:[0,4,7],kick:[0,4,8,12],snare:[4,12],hat:[2,6,10,14],bass:[0,4,8,12],melody:[12,null,7,null,4,null,7,null,12,null,7,null,4,null,7,null],swing:0},
  {id:'rush',name:'Rush Mode',bpm:136,style:'Fast Electronic',wave:'triangle',roots:[45,53,48,55],chord:[0,3,7],kick:[0,4,8,12,15],snare:[4,12],hat:[0,2,4,6,8,10,12,14,15],bass:[0,2,6,8,10,14],melody:[12,null,7,null,null,10,null,7,12,null,15,null,null,10,null,7],swing:0},
- {id:'night',name:'Night Drive',bpm:112,style:'Soft Synth',wave:'sine',roots:[47,43,50,45],chord:[0,3,7,14],kick:[0,8],snare:[4,12],hat:[2,6,10,14],bass:[0,8,14],melody:[null,null,null,null,14,null,null,null,null,null,null,null,7,null,null,null],swing:.04}
+ {id:'night',name:'Night Drive',bpm:112,style:'Soft Synth',wave:'sine',roots:[47,43,50,45],chord:[0,3,7,14],kick:[0,8],snare:[4,12],hat:[2,6,10,14],bass:[0,8,14],melody:[null,null,null,null,14,null,null,null,null,null,null,null,7,null,null,null],swing:.04},
+ {id:'sunlight',name:'Sunlight on the Dash',bpm:120,style:'Acoustic Pop / Imported MP3',custom:true,loopStart:.245,loopEnd:60.006}
 ];
